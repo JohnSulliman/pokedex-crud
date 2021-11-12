2d92b3edc0047eb4f906ef9f0ccdd585776bf05b
Sulliman <johnatantc@hotmail.com>
2021-11-11 23:28:43 -0300

    Página incial do pokedex-crud

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded());
+
+app.get("/", (req, res) => { 
+  
+  res.render("index", {
+     
+    },
+  );
+    
+}); 
+
+app.listen ( port, () => 
+  console.log(`Servidor rodando em localhost:${port}`));
