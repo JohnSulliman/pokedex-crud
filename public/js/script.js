e3b3e62b296df63252fe797cb3188c15fc701ddf
Sulliman <johnatantc@hotmail.com>
2021-11-17 21:53:21 -0300

    Página base

---
+++ public/js/script.js
@@ -0,0 +1,10 @@
+// const home = document.querySelector(".menuHome")
+// const register = document.querySelector(".menuRegister")
+
+// home.addEventListener('click', function() {
+
+// })
+
+// register.addEventListener('click', function() {
+
+// })
