68756e082f217f2034233868d31686f0f4dea167
Sulliman <johnatantc@hotmail.com>
2021-11-24 17:39:31 -0300

    Removendo comentário do arquivo index.js da pasta database

--- database/index.js
+++ database/index.js
@@ -1,12 +1,6 @@
 require('dotenv').config();
 const Sequelize = require("sequelize");
 
-// const sequelize = new Sequelize(process.env.DB_BASE, process.env.DB_USER,process.env.DB_PASS, {
-//   host:  process.env.DB_HOST,
-//   port: process.env.DB_PORT,
-//   dialect: 'postgres'
-// });
-
 const sequelize = new Sequelize(process.env.DATABASE_URL, {     
    dialect: 'postgres',
    protocol: 'postgres',     
